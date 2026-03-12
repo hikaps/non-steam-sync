6d52e47b02c9b1d66b197060b1cf52b3c26da701
hikaps <dev@null>
2026-03-12 13:43:48 -0700

    test(core): add action preservation tests for merge utility
    - PreservesExistingFileActionWhenAddingSteamUrl
    - PreservesMultipleExistingActionsWhenAddingSteamUrl
    - HandlesNullGameActionsGracefully
    - HandlesEmptyGameActionsGracefully
    - UpdatesTrackingPathOnExistingSteamAction
    - DoesNotModifyExistingActionProperties

--- tests/ShortcutsTests/SteamActionUtilitiesTests.cs
+++ tests/ShortcutsTests/SteamActionUtilitiesTests.cs
@@ -98,4 +98,136 @@ public class SteamActionUtilitiesTests
         Assert.Contains(otherSteam, updated);
         Assert.Equal(expectedUrl, steamAction.Path);
     }
+
+    [Fact]
+    public void PreservesExistingFileActionWhenAddingSteamUrl()
+    {
+        var fileAction = new GameAction
+        {
+            Name = "Play",
+            Type = GameActionType.File,
+            Path = "game.exe",
+            Arguments = "-windowed",
+            WorkingDir = @"C:\Games",
+            TrackingMode = TrackingMode.Process,
+            TrackingPath = @"C:\Games\game.exe",
+            IsPlayAction = true
+        };
+
+        var expectedUrl = $"{Constants.SteamRungameIdUrl}555666";
+        var trackingPath = @"C:\Tracking";
+        var changed = GameActionUtilities.EnsureSteamLaunchAction(new List<GameAction> { fileAction }, expectedUrl, trackingPath, out var updated, out var steamAction);
+
+        Assert.True(changed);
+        Assert.Equal(2, updated.Count);
+        Assert.Same(fileAction, updated[1]);
+        Assert.Equal("Play", fileAction.Name);
+        Assert.Equal(GameActionType.File, fileAction.Type);
+        Assert.Equal("game.exe", fileAction.Path);
+        Assert.Equal("-windowed", fileAction.Arguments);
+        Assert.Equal(@"C:\Games", fileAction.WorkingDir);
+        Assert.Equal(TrackingMode.Process, fileAction.TrackingMode);
+        Assert.Equal(@"C:\Games\game.exe", fileAction.TrackingPath);
+        Assert.False(fileAction.IsPlayAction);
+        Assert.Same(steamAction, updated[0]);
+    }
+
+    [Fact]
+    public void PreservesMultipleExistingActionsWhenAddingSteamUrl()
+    {
+        var fileAction = new GameAction { Name = "File", Type = GameActionType.File, Path = "game.exe", IsPlayAction = true };
+        var urlAction = new GameAction { Name = "Help", Type = GameActionType.URL, Path = "https://example.com", IsPlayAction = false };
+        var customAction = new GameAction { Name = "Custom", Type = GameActionType.Script, Path = "script.ps1", IsPlayAction = false };
+
+        var expectedUrl = $"{Constants.SteamRungameIdUrl}777888";
+        var trackingPath = @"C:\Games";
+        var changed = GameActionUtilities.EnsureSteamLaunchAction(new List<GameAction> { fileAction, urlAction, customAction }, expectedUrl, trackingPath, out var updated, out var steamAction);
+
+        Assert.True(changed);
+        Assert.Equal(4, updated.Count);
+        Assert.Contains(fileAction, updated);
+        Assert.Contains(urlAction, updated);
+        Assert.Contains(customAction, updated);
+        Assert.Contains(steamAction, updated);
+    }
+
+    [Fact]
+    public void HandlesNullGameActionsGracefully()
+    {
+        var expectedUrl = $"{Constants.SteamRungameIdUrl}111222";
+        var trackingPath = @"C:\Games";
+
+        var changed = GameActionUtilities.EnsureSteamLaunchAction(null, expectedUrl, trackingPath, out var updated, out var steamAction);
+
+        Assert.True(changed);
+        Assert.Single(updated);
+        Assert.Same(steamAction, updated[0]);
+    }
+
+    [Fact]
+    public void HandlesEmptyGameActionsGracefully()
+    {
+        var expectedUrl = $"{Constants.SteamRungameIdUrl}333444";
+        var trackingPath = @"C:\Games";
+
+        var changed = GameActionUtilities.EnsureSteamLaunchAction(new List<GameAction>(), expectedUrl, trackingPath, out var updated, out var steamAction);
+
+        Assert.True(changed);
+        Assert.Single(updated);
+        Assert.Same(steamAction, updated[0]);
+    }
+
+    [Fact]
+    public void UpdatesTrackingPathOnExistingSteamAction()
+    {
+        var existingSteam = new GameAction
+        {
+            Name = Constants.PlaySteamActionName,
+            Type = GameActionType.URL,
+            Path = $"{Constants.SteamRungameIdUrl}999000",
+            TrackingMode = TrackingMode.Directory,
+            TrackingPath = @"C:\Old",
+            IsPlayAction = true
+        };
+
+        var trackingPath = @"C:\New";
+        var changed = GameActionUtilities.EnsureSteamLaunchAction(new List<GameAction> { existingSteam }, existingSteam.Path, trackingPath, out var updated, out var steamAction);
+
+        Assert.True(changed);
+        Assert.Same(existingSteam, steamAction);
+        Assert.Equal(trackingPath, steamAction.TrackingPath);
+        Assert.Single(updated);
+    }
+
+    [Fact]
+    public void DoesNotModifyNonPlayActionPropertiesOfExistingActions()
+    {
+        var action = new GameAction
+        {
+            Name = "Primary",
+            Type = GameActionType.File,
+            Path = "primary.exe",
+            Arguments = "-fullscreen",
+            WorkingDir = @"C:\Games",
+            TrackingMode = TrackingMode.Process,
+            TrackingPath = @"C:\Games\primary.exe",
+            IsPlayAction = true
+        };
+
+        var expectedUrl = $"{Constants.SteamRungameIdUrl}444555";
+        var trackingPath = @"C:\Tracking";
+        var changed = GameActionUtilities.EnsureSteamLaunchAction(new List<GameAction> { action }, expectedUrl, trackingPath, out var updated, out var steamAction);
+
+        Assert.True(changed);
+        Assert.Same(action, updated[1]);
+        Assert.Equal("Primary", action.Name);
+        Assert.Equal(GameActionType.File, action.Type);
+        Assert.Equal("primary.exe", action.Path);
+        Assert.Equal("-fullscreen", action.Arguments);
+        Assert.Equal(@"C:\Games", action.WorkingDir);
+        Assert.Equal(TrackingMode.Process, action.TrackingMode);
+        Assert.Equal(@"C:\Games\primary.exe", action.TrackingPath);
+        Assert.False(action.IsPlayAction);
+        Assert.Same(steamAction, updated[0]);
+    }
 }
